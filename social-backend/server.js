e5c07aec56d41384ab189f9d2ccd30e0ff006157
JatinSharmacs21 <jatinsharmacs21@gmail.com>
2026-05-11 17:32:32 +0530

    Resolved

--- social-backend/server.js
+++ social-backend/server.js
@@ -3,10 +3,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 const cors = require("cors");
 
-app.use(cors({
-  origin: "*", 
-  credentials: true
-}));
+
 
 const connectDB = require("./config/db");
 
@@ -26,7 +23,10 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*", 
+  credentials: true
+}));
 app.use(express.json());
 
 // TEST ROUTE
