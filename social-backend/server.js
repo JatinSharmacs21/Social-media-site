7721208d8af92154af2508718ab4ab1ea7fc58a6
JatinSharmacs21 <jatinsharmacs21@gmail.com>
2026-05-11 17:29:30 +0530

    added debug logs

--- social-backend/server.js
+++ social-backend/server.js
@@ -153,3 +153,11 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+process.on("uncaughtException", (err) => {
+  console.log("UNCAUGHT EXCEPTION:", err);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log("UNHANDLED REJECTION:", err);
+});
